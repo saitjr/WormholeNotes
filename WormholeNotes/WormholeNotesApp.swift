ff3009d3da3d416de07e9493f541d74f1edf4168
tangjiarong <tangjiarong@bytedance.com>
2023-01-31 13:09:38 +0800

    Initial Commit

---
+++ WormholeNotes/WormholeNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WormholeNotesApp.swift
+//  WormholeNotes
+//
+//  Created by tangjiarong on 2023/1/31.
+//
+
+import SwiftUI
+
+@main
+struct WormholeNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
