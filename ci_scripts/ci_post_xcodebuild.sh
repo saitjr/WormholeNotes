18aa379731ac7177f88cf2ecf34610ef2a5f5940
tangjiarong <tangjr.work@gmail.com>
2023-02-01 11:13:15 +0800

    feat: add custom scripts

---
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  ci_post_xcodebuild.sh
+#  WormholeNotes
+#
+#  Created by tangjiarong on 2023/2/1.
+#  
+
+echo "run ci_post_xcodebuild.sh"
---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  ci_pre_xcodebuild.sh
+#  WormholeNotes
+#
+#  Created by tangjiarong on 2023/2/1.
+#  
+
+echo "run ci_pre_xcodebuild.sh"
